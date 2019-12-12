6757fbcafc6b1bc5ba40a16f1714d2fc1616f7b6
SorinGabriel02 <lupu_sg@yahoo.com>
2019-12-12 17:43:35 +0200

    setting up authentification

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "425192389890-l2avcc8dvj2lsg1ppo2726khdbkf4u0i.apps.googleusercontent.com",
+        scope: "email"
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
